e64e9fc0bb1fecb373cb6a77ded68d8fbd75d955
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2024-09-05 15:50:29 -0300

    Projeto Finalizado

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,58 +1,62 @@
-import React from 'react'
-import { FaUser, FaLock } from 'react-icons/fa'
-import { useState } from 'react'
-import "./Login.css"
+import { useState } from "react";
+import { FaUser, FaLock } from "react-icons/fa";
+import "./Login.css";
 
 const Login = () => {
+  // Estados para armazenar as entradas do usuário
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState("")
-    const [password, setPassword] = useState("")
+  // Função que é chamada quando o formulário é enviado
+  const handleSubmit = (event) => {
+    // Impede que a página seja recarregada
+    event.preventDefault();
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
+    // Faz o console log das credenciais do usuário
+    console.log("Dados de Login:", { username, password });
+  };
 
-        console.log("teste", setUsername, setPassword);
-
-
-        console.log("Envio");
-    }
-
-    return (
-        <div className='Container'>
-            <form onSubmit={handleSubmit}>
-                <h1>Acesse o Sistema</h1>
-                <div>
-                    <input 
-                    type="email" 
-                    placeholder='email' 
-                    onChange={(e) => setUsername(e.target.value)} 
-                    />
-                    <FaUser />
-                </div>
-                <div>
-                    <input 
-                    type="password" 
-                    placeholder='password' 
-                    />
-                    <FaLock />
-                </div>
-
-                <div className="recall-forget">
-                    <label >
-                        <input type="checkbox" />
-                        Lembre de mim
-                        <a href="#">Esqueceu a Senha</a>
-                    </label>
-                </div>
-                <button>Entrar</button>
-                <div className="signup-link">
-                    <p>
-                        Não tem conta? <a href="#">Registrar</a>
-                    </p>
-                </div>
-            </form>
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Acesse o sistema</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
         </div>
-    )
-}
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
 
-export default Login
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label>
+          <a href="#">Esqueceu sua senha?</a>
+        </div>
+        <button type="submit">Login</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registar</a>{" "}
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
